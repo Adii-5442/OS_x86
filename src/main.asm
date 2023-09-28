403a489f9ac8508c6162606f5bd02c4b38d8f238
Adi-5442 <adi.sh5442@gmail.com>
2023-09-28 19:23:55 +0530

    Address calculation and Offset values

--- src/main.asm
+++ src/main.asm
@@ -1,11 +1,17 @@
 org 0x7C00
 bits 16
 
+;The main Function
 main:
 	hlt
+
 
 .halt:
 	jmp .halt
 
-times 510-($-$$) db 0
+;$ - means the current location
+;$$ means the address of start of the section(or sector)
+; therefore after defining all this, the current location is at the end of prograam, and $-$$ gives the length of the program
+
+times 510-($-$$) db 0 ; times is used for
 dw 0AA55h
