e4d5360828c0e9479dee514131edf68e470c87b3
Adi-5442 <adi.sh5442@gmail.com>
2023-09-28 22:41:50 +0530

    Boot message rendered in OS

--- src/main.asm
+++ src/main.asm
@@ -1,13 +1,62 @@
-org 0x7C00
+org 0x7C00 ;This is memory address where BIOS will be looking for the
 bits 16
 
-;The main Function
+
+
+%define ENDL 0x0D, 0x0A
+
+start:
+	jmp main
+
+
+
+;Prints a string to the screen.
+;Params:
+;	-ds:si points to the string
+
+puts:
+	; save registers we will modify
+	push si
+	push ax
+
+.loop:
+	lodsb        ;loads next character in al
+	or al,al	;verify if next character is Null (taking bitwise or of al with al to check for nullability)
+	jz .done
+
+	mov ah,0x0e ;call bios interrupt
+	int 0x10
+
+	jmp .loop
+
+.done:
+	pop ax
+	pop si
+	ret
+
+
+
 main:
-	hlt
+	;data segments setup
+	mov ax, 0 		;can't write to ds/es directly
+	mov ds, ax
+	mov es, ax
+
+	;setup stack
+	mov ss, ax
+	mov sp, 0x7C00 ; stack grows downwards where we are loaded in memory
+
+	;PRINT MESSAGE
+	mov si , msg_hello
+	call puts
+
+
 
 
 .halt:
 	jmp .halt
+
+msg_hello: db 'Hello World!',ENDL,0
 
 ;$ - means the current location
 ;$$ means the address of start of the section(or sector)
